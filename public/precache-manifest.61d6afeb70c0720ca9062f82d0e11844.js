6eebd26c2bfbf2b9e906fa4c6bf36a33b8727882
Mustafa Anas <ad16mkha@uwcad.it>
2019-04-30 06:06:35 +0300

    Heroku Commit

---
+++ public/precache-manifest.61d6afeb70c0720ca9062f82d0e11844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1981552843985538ebd0bdd4d48312e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76374fe773d519878e23",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "cb2981ad696b80ed4eaa",
+    "url": "/static/js/2.f906ccce.chunk.js"
+  },
+  {
+    "revision": "76374fe773d519878e23",
+    "url": "/static/js/main.5bb95ecd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "acb89e4a6af2c379a62ed023016a827b",
+    "url": "/static/media/logo.acb89e4a.png"
+  }
+]);
